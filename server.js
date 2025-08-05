2b89ba13d22f23511aa5689198b69a8fafb9cd8c
Igor <igor.levochkin@xrtc.io>
2025-08-05 12:01:18 +0700

    Increase port from 3016 to 3019

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid'); // Import uuid
 
 const app = express();
-const port = 3007;
+const port = 3019;
 
 // Store active terminal sessions
 const sessions = new Map(); // Map to store sessionID -> { ptyProcess, ws, timeoutId, buffer, projectName }
